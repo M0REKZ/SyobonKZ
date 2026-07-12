eecc5f996f32b9988a629d01dd4ce6978e422555
+KZ <60852359+M0REKZ@users.noreply.github.com>
2026-07-11 23:51:21 -0400

    adding logos

--- global_vars.h
+++ global_vars.h
@@ -79,7 +79,7 @@ extern SDL_Surface *Sliced_GFX[161][8];
 extern SDL_Surface *Main_GFX[51];
 
 // +KZ: i added these
-#define MAIN_GFX_KZ_MAX 14
+#define MAIN_GFX_KZ_MAX 16
 #define SLICED_GFX_KZ_MAX 6
 #define SOUNDS_KZ_MAX 2
 extern SDL_Surface *Main_GFX_KZ[MAIN_GFX_KZ_MAX]; //+KZ
@@ -463,9 +463,11 @@ extern bool StartFullScreenFlag;
 enum class ESyobonActionGame : int
 {
     SYOBON_ACTION_1_AND_2 = 0,
-    SYOBON_ACTION_3,
-    KAIZO_SYOBON,
-    SYOBONKZ_TRUE_ACTION, //+KZ
+    SYOBON_ACTION_3 = 300,
+    SYOBON_NO_ACTION_JIN = 700,
+    SYOBON_NO_ACTION_TAKUMI = 1000,
+    KAIZO_SYOBON = 1500,
+    SYOBONKZ_TRUE_ACTION = 3000, //+KZ
 
     FIRST = SYOBON_ACTION_1_AND_2,
     LAST = SYOBONKZ_TRUE_ACTION,
